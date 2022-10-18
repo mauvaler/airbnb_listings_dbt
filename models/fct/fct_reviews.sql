7df3ff0853d1893fb7b63b37db1b18234d53b903
maulvaler <mau.valer@icloud.com>
2022-10-18 19:55:09 +0100

    Add identation on fct_reviews

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -1,17 +1,27 @@
 {{ config(
-    materialized = 'incremental',
-    on_schema_change='fail'
-    )
-}}
+  materialized = 'incremental',
+  on_schema_change = 'fail'
+) }}
+
 WITH src_reviews AS (
-  SELECT * FROM {{ ref('src_reviews') }}
-) 
-SELECT 
-  {{ dbt_utils.surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} AS review_id,
-  *
-FROM src_reviews
-WHERE review_text is not null
+
+  SELECT
+    *
+  FROM
+    {{ ref('src_reviews') }}
+)
+SELECT
+  {{ dbt_utils.surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} AS review_id,*
+FROM
+  src_reviews
+WHERE
+  review_text IS NOT NULL
 
 {% if is_incremental() %}
-  AND review_date > (select max(review_date) from {{ this }})
+AND review_date > (
+  SELECT
+    MAX(review_date)
+  FROM
+    {{ this }}
+)
 {% endif %}
