6de237a30e14929fcb04f09637c047c95101339a
maulvaler <mau.valer@icloud.com>
2022-10-18 16:52:58 +0100

    Add identation

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -6,7 +6,8 @@
 WITH src_reviews AS (
   SELECT * FROM {{ ref('src_reviews') }}
 ) 
-SELECT {{ dbt_utils.surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} AS review_id,
+SELECT 
+  {{ dbt_utils.surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} AS review_id,
   *
 FROM src_reviews
 WHERE review_text is not null
